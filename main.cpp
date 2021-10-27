f46ae6bf342e77bd68a16de17f71071f28de0a76
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-10-27 18:22:10 +0200

    Shape functoins added.

--- main.cpp
+++ main.cpp
@@ -72,32 +72,32 @@ class Element{
   };
 };
 
-class Grid{
+class PixelCenterGrid{
  public:
-  int num_elements_x_;
-  int num_elements_y_;
-  double element_width_;
-  double element_height_;
+  int n_pixels_x_;
+  int n_pixels_y_;
+  double pixel_width_;
+  double pixel_height_;
   double grid_base_value_;
-  std::vector<Element> elements_;
+  std::vector<Element> pixels_;
   
-  Grid(int num_elements_x, int num_elements_y, double element_width, double element_height, double grid_base_value){
-    num_elements_x_ = num_elements_x;
-    num_elements_y_ = num_elements_y;
-    element_width_ = element_width;
-    element_height_ = element_height;
+  PixelCenterGrid(int n_pixels_x, int n_pixels_y, double pixel_width, double pixel_height, double grid_base_value){
+    n_pixels_x_ = n_pixels_x;
+    n_pixels_y_ = n_pixels_y;
+    pixel_width_ = pixel_width;
+    pixel_height_ = pixel_height;
     grid_base_value_= grid_base_value;
     double pos_x, pos_y;
 
-    for(int col = 0; col < num_elements_y_; col++){
-      for(int row = 0; row < num_elements_x_; row++){
-        pos_x = element_width_/2 + col * element_width_;
-        pos_y = element_height_/2 + row * element_height_;
-        elements_.push_back(Element(row + num_elements_x_ * col + 1, grid_base_value_, pos_x, pos_y, element_width_, element_height_, 0));
+    for(int col = 0; col < n_pixels_y_; col++){
+      for(int row = 0; row < n_pixels_x_; row++){
+        pos_x = pixel_width_/2 + col * pixel_width_;
+        pos_y = pixel_height_/2 + row * pixel_height_;
+        pixels_.push_back(Element(row + n_pixels_x_ * col + 1, grid_base_value_, pos_x, pos_y, pixel_width_, pixel_height_, 0));
       }
     }
   };
-  ~Grid(){};
+  ~PixelCenterGrid(){};
 };
 
 PointCartesian ConvertPolarToCartesian(PointPolar p);
@@ -117,16 +117,93 @@ PointCartesian ConvertPolarToCartesian(PointPolar p);
  *     *     *     *     *
  *     *
  *     y
+ * 
+ *   > _____V______V______V_______   0---x      0---a
+ *    |  |      |      |      |  |   |          |
+ *    - C01 -- C02 -- C03 -- C04 -   y          b
+ *   >|  |  E1  |  E2  |  E3  |  |   
+ *    - C05 -- C06 -- C07 -- C08 -   
+ *   >|  |  E4  |  E5  |  E6  |  |
+ *    - C09 -- C10 -- C11 -- C12 -
+ *   >|  |      |      |      |  |
+ * 
+ * 
+ * E1: 1,2,6,5
+ * E2: 2,3,7,6
+ * En: n,n+1, d * w + 1, d*w 
+ * 
+ * 
 **/
 int ConvertCartesianToElementNumber(PointCartesian p);
 
-int main(){
-  Grid my_grid(3,2,1.0,1.0,101);
-  for (Element e : my_grid.elements_){
-    std::cout << e.id_ << " at (" << e.pos_x_ << ", " << e.pos_y_ << ") has a value of " << e.value_ << "\n";
+
+class ElementSecondary{
+ public:
+  double pos_x_;
+  double pos_y_;
+  double width_;
+  double height_;
+  //int node_ids[4];
+  double node_values_[4];;
+  ElementSecondary(double pos_x, double pos_y, double width, double height, double node_values, int node_ids){
+    pos_x_ = pos_x;
+    pos_y_ = pos_y;
+    width_ = width;
+    height_ = height;
+    //node_ids_ = node_ids
+    for (int i = 0; i < 4; i++ ){
+      node_values_[i] = 101+i;
+    }
+  };
+  double valueAt(double x, double y){
+    double a = (x - pos_x_) / (width_/2);
+    double b = (y - pos_y_) / (height_/2);
+    double w1 = 0.25 * (1 - a) * (1 - b);
+    double w2 = 0.25 * (1 + a) * (1 - b);
+    double w3 = 0.25 * (1 + a) * (1 + b);
+    double w4 = 0.25 * (1 - a) * (1 + b);
+    return w1 * node_values_[0] + w2 * node_values_[1] + w3 * node_values_[2] + w4 * node_values_[3];
   }
+  
+};
+
+
+int main(){
+  PixelCenterGrid my_pixel_center_grid(3,2,1.0,1.0,101);
+  for (Element pixel_center : my_pixel_center_grid.pixels_){
+    std::cout << "Pixel " << pixel_center.id_ << " at (" << pixel_center.pos_x_ << ", " << pixel_center.pos_y_ << ") has a value of " << pixel_center.value_ << "\n";
+  }
+
+  // Test a single element
+  ElementSecondary temp(1.0, 1.0, 4.0, 4.0, 0, 0);
+  std::cout << temp.valueAt(1.0, 1.0) << std::endl;
+  std::cout << temp.valueAt(3.0, 3.0) << std::endl;
+
+  // Load a image
+  double image[10][10] = {
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0}
+  };
+
+  // r = 1;
+  // theta = 1;
+  // x = fx(r, theta);
+  // y = fy(r, theta);
+  // element_id = g(grid_elements, x,y);
+  // z_value_at_xy = h(grid_elements.elements[element_id], x,y);
+
 }
 
 int ConvertCartesianToElementNumber(){
 
 }
+
+
