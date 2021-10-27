87788e7c97bb29a763ace4fff5e9243deddfed4d
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-10-27 13:16:18 +0200

    An example grid constructed.

--- main.cpp
+++ main.cpp
@@ -7,17 +7,18 @@
  * How does it works:
  *   1. Grid configuration:
  *     1.1. Element numbering
+ *       - Boundary is defined by Element 0.
  * 
  *     y
  *     ^
  *     |
- *     |    *     *     *     *
+ *     |  0 *  0  *  0   *  0  *
  *     |  * * * * * * * * * * * *
- *     |    *  1  *  2  *  3  *
+ *     |  0 *  1  *  2  *  3  *
  *     |  * * * * * * * * * * * *
- *     |    *  4  *  5  *  6  *
+ *     |  0 *  4  *  5  *  6  *
  *     |  * * * * * * * * * * * *
- *     |    *  7  *  8  *  9  *
+ *     |  0 *  7  *  8  *  9  *
  *     |  * * * * * * * * * * * *
  *     |    *     *     *     *
  *     |
@@ -29,13 +30,18 @@
  **/
 
 #include <iostream>
+#include <vector>
+#include <iterator>
 #include <string>
 
 class PointCartesian{
  public:
   double x_;
   double y_;
-  PointCartesian();
+  PointCartesian(double x, double y){
+    x_ = x;
+    y_ = y;
+  };
 };
 
 class PointPolar{
@@ -47,16 +53,80 @@ class PointPolar{
 
 class Element{
  public:
+  int id_;
+  double value_;
+  double pos_x_;
+  double pos_y_;
   double width_;
   double height_;
-  double value_;
-  Element();
+  int adj_elements_[8]; // Conventional matrix notaion excluding #5 (see header comment 1.1)
+
+  Element(int id, double value, double pos_x, double pos_y, double width, double height, int adj_element){
+    id_ = id;
+    value_ = value;
+    pos_x_= pos_x;
+    pos_y_= pos_y;
+    width = width;
+    height_ = height;
+    //adj_elements_ = adj_elements_;
+  };
+};
+
+class Grid{
+ public:
+  int num_elements_x_;
+  int num_elements_y_;
+  int element_width_;
+  int element_height_;
+  double grid_base_value_;
+  std::vector<Element> elements_;
+  
+  Grid(int num_elements_x, int num_elements_y, double element_width, double element_height, double grid_base_value){
+    num_elements_x_ = num_elements_x;
+    num_elements_y_ = num_elements_y;
+    element_width_ = element_width;
+    element_height_ = element_height;
+    grid_base_value_= grid_base_value;
+    double pos_x, pos_y;
+
+    for(int col = 0; col < num_elements_y_; col++){
+      for(int row = 0; row < num_elements_x_; row++){
+        pos_x = element_width_/2 + col * element_width_;
+        pos_y = element_height_/2 + row * element_height_;
+        elements_.push_back(Element(row + num_elements_x_ * col + 1, grid_base_value_, pos_x, pos_y, element_width_, element_height_, 0));
+      }
+    }
+  };
+  ~Grid(){};
 };
 
 PointCartesian ConvertPolarToCartesian(PointPolar p);
+
+/**
+ * Returns the element number of a given cartesian point p(x,y)
+ * 
+ * Example:
+ *
+ *     * * * * * * * * * * * * * x
+ *     *  1  *  2  *  3  *
+ *     * * * * * * * * * * *
+ *     *  4  *  5  *  6  *
+ *     * * * * * * * * * * *
+ *     *  7  *  8  *  9  *
+ *     * * * * * * * * * * *
+ *     *     *     *     *
+ *     *
+ *     y
+**/
 int ConvertCartesianToElementNumber(PointCartesian p);
 
-int main()
-{
-  std::cout << "Test"  << std::endl;
+int main(){
+  Grid my_grid(3,2,1,1,101);
+  for (Element e : my_grid.elements_){
+    std::cout << e.id_ << " at (" << e.pos_x_ << ", " << e.pos_y_ << ") has a value of " << e.value_ << "\n";
+  }
+}
+
+int ConvertCartesianToElementNumber(){
+
 }
