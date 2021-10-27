d2fcff4588cec4ec3586642907fa9ac970a9ddc2
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-10-27 08:44:23 +0200

    Comment added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,33 @@
+/**
+ * File   main.cpp
+ * 
+ * This code converts a 2D discrete function from Cartesian representation to Polar 
+ * one using node interpolation.
+ * 
+ * How does it works:
+ *   1. Grid configuration:
+ *     1.1. Element numbering
+ * 
+ *     y
+ *     ^
+ *     |
+ *     |    *     *     *     *
+ *     |  * * * * * * * * * * * *
+ *     |    *  1  *  2  *  3  *
+ *     |  * * * * * * * * * * * *
+ *     |    *  4  *  5  *  6  *
+ *     |  * * * * * * * * * * * *
+ *     |    *  7  *  8  *  9  *
+ *     |  * * * * * * * * * * * *
+ *     |    *     *     *     *
+ *     |
+ *     . ----------------------------> x
+ * 
+ *     1.2. Node numbering
+ *     1.3. Element adjacency matrix
+ *     1.4. Node adjacency matrix
+ **/
+
 #include <iostream>
 #include <string>
 
